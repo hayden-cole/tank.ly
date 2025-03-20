22d09baaec1e9adef93f705e7be6efa9de2d5cd7
Hayden Cole <haydencole@protonmail.com>
2025-03-20 14:54:22 -0700

    Implement pull-to-refresh on tank list

--- app/src/main/java/com/demo/tankly/ui/tanklist/TankListScreen.kt
+++ app/src/main/java/com/demo/tankly/ui/tanklist/TankListScreen.kt
@@ -1,5 +1,6 @@
 package com.demo.tankly.ui.tanklist
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,16 +8,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -44,24 +51,27 @@ fun TankItem(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TankListScreen(navController: NavController, viewModel: TankListViewModel = viewModel()) {
     val tankList = viewModel.tankList.observeAsState(emptyList())
+    val context = LocalContext.current
+    var isRefreshing by remember { mutableStateOf(false) }
 
     Scaffold(
-        topBar = { Text("Tank List") }
+        topBar = { Text("Tank List", modifier = Modifier.padding(16.dp)) }
     ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(onClick = { viewModel.refreshTanks() }) {
-                Text("Refresh Tanks")
-            }
+        PullToRefreshBox (
+            isRefreshing = isRefreshing,
+            onRefresh = {
+                isRefreshing = true
+                viewModel.refreshTanks()
 
-            LazyColumn {
+                Toast.makeText(context, "You have the latest data.", Toast.LENGTH_SHORT).show()
+            },
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            LazyColumn(Modifier.fillMaxSize()) {
                 items(tankList.value) { tank ->
                     TankItem(
                         tank = tank,
@@ -73,6 +83,9 @@ fun TankListScreen(navController: NavController, viewModel: TankListViewModel = 
     }
 
     LaunchedEffect(Unit) {
-        viewModel.refreshTanks()
+        viewModel.tankList.observeForever {
+            isRefreshing = false
+        }
+//        viewModel.refreshTanks()
     }
 }
